3cf30a1ed99b921d6e1848510d0cdd93e513289b
Hannah Marumoto <u1320619@utah.edu>
2024-12-13 20:47:48 -0700

    run script fixed

--- Perceptron/run.sh
+++ Perceptron/run.sh
@@ -1,1 +1,3 @@
-python3 NeuralNetwork.py
+python3 Perceptron.py bank-note/bank-note/train.csv bank-note/bank-note/test.csv standard 10
+python3 Perceptron.py bank-note/bank-note/train.csv bank-note/bank-note/test.csv averaged 10
+python3 Perceptron.py bank-note/bank-note/train.csv bank-note/bank-note/test.csv voted 10
